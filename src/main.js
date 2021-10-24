6e7c70b79ea6b31bc0ddd776fe1d5be3f2ef54f1
hsq794 <1993610971@qq.con>
2021-10-24 15:53:26 +0800

    add files

--- src/main.js
+++ src/main.js
@@ -1,6 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
+import ElementUI from 'element-ui'
+import locale from 'element-ui/lib/locale/lang/en'
+import './styles.scss'
+import ElementUI from 'element-ui'
+import './styles.scss'
+
+Vue.use(ElementUI)
+
+Vue.use(ElementUI, { locale })
 
 Vue.config.productionTip = false
 
